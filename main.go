22528172a855f54421342660837e223cd082a613
Hikaru Watanabe <hikaru.watanabe@dena.com>
2019-05-18 16:28:35 +0900

    get rid of fetcher and the codes call it

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/jessevdk/go-flags"
 	"gopkg.in/go-playground/validator.v9"
 
-	"github.com/xruins/route53-updater/ipfetcher"
 	"github.com/xruins/route53-updater/route53"
 )
 
@@ -18,8 +17,6 @@ type cmdOptions struct {
 	Domain       string `short:"d" long:"domain" description:"domain name to notify." validator:"required,fqdn"`
 	IPV4Address  string `short:"4" long:"ipv4" description:"IPV4 address to notify. specify 'omit' to skip notification of IPv4 address." validator:"ipv4"`
 	IPV6Address  string `short:"6" long:"ipv6" description:"IPV6 address to notify. specify 'omit' to skip notification of IPv4 address." validator:"ipv6"`
-	AutoFetchV4  bool   `long:"auto-fetch-v4" description:"whether gets remote IP address from remote server. enabled by default."`
-	AutoFetchV6  bool   `long:"auto-fetch-v6" description:"If true, notification for IPv4 address will not be execute without --ipv6 flag."`
 	HostedZoneID string `short:"z" long:"hosted-zone-id" description:"HostedZoneID of Route53." validator:"required,alphanum"`
 	TTL          int32  `short:"t" long:"ttl" description:"time to live in second for DNS records." validator:"min=0"`
 }
@@ -42,51 +39,15 @@ func main() {
 		os.Exit(1)
 	}
 
-	if options.IPV4Address == "" && !options.AutoFetchV4 {
-		fmt.Fprintf(os.Stderr, "--ipv4 option is required when --auto-fetch-v4 option is false.\n")
+	if options.IPV4Address == "" && options.IPV6Address == "" {
+		fmt.Fprintln(os.Stderr, "either --ipv4 or --ipv6 required.")
 		os.Exit(1)
 	}
 
-	if options.IPV6Address == "" && !options.AutoFetchV6 {
-		fmt.Fprintf(os.Stderr, "--ipv6 option is required when --auto-fetch-v6 option is false.\n")
-		os.Exit(1)
-	}
+	ipv4 := net.ParseIP(options.IPV4Address)
+	ipv6 := net.ParseIP(options.IPV6Address)
 
 	ctx := context.Background()
-
-	var ipf ipfetcher.IPFetcher
-	ipf = &ipfetcher.IfconfigIoFetcher{}
-
-	var ipv4 net.IP
-	if !options.AutoFetchV4 {
-		ipv4 = net.ParseIP(options.IPV4Address)
-	} else {
-		ipv4, err = ipf.FetchIPv4(ctx)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to fetch IPv4 address. err: %s\n", err)
-		}
-		if ipv4 == nil {
-			fmt.Fprintf(os.Stderr, "fetched malformed IPv4 address. address: %s\n", ipv4)
-		}
-	}
-
-	var ipv6 net.IP
-	if !options.AutoFetchV6 {
-		ipv6 = net.ParseIP(options.IPV6Address)
-	} else {
-		ipv6, err = ipf.FetchIPv6(ctx)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to fetch IPv6 address. err: %s\n", err)
-		}
-		if ipv6 == nil {
-			fmt.Fprintf(os.Stderr, "fetched malformed IPv6 address. address: %s\n", ipv6)
-		}
-	}
-
-	if ipv4 == nil && ipv6 == nil {
-		fmt.Fprintf(os.Stderr, "could not fetch both of ipv4 address and ipv6 one. abort.\n")
-		os.Exit(1)
-	}
 
 	r53 := &route53.Route53{
 		Domain:       options.Domain,
